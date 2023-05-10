868a69b959c7c0d9b6864c3d01c6ca7ffeb46183
AlexanderARodin <rodin_aa@inbox.ru>
2023-05-11 02:20:43 +0300

    saving

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,7 @@ async fn main() {
     println!("started..");
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
         println!("inside!!!: {:?}", msg.text());
-        bot.send_message(msg.chat.id, "putin xuiylo").await?;
-        bot.send_message(msg.chat.id, "Artem - molotoGGGG").await?;
+        bot.send_message(msg.chat.id, "kill me, plz").await?;
         Ok(())
     })
     .await;
