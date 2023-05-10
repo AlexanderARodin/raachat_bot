cd40767ad9c0e02c73ad95fe3ad655512a37fb38
AlexanderARodin <rodin_aa@inbox.ru>
2023-05-10 03:25:42 +0300

    saving

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+use std::env;
+use std::process;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("ERROR: there is no Token in command line");
+        process::exit(1);
+    }else{
+        println!("start under token: {}", args[1] );
+        start_bot( args[1].clone() );
+    }
+}
+
+fn start_bot( bot_token: String ) {
+    println!( "{}", bot_token );
+
 }
