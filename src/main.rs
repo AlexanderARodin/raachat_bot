bec051e2786988302d03e9ecb0651b2b176b715f
AlexanderARodin <rodin_aa@inbox.ru>
2023-05-11 23:57:18 +0300

    saving

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use teloxide::prelude::*;
 
+
+static mut senderID: ChatId = teloxide::prelude::ChatId(0);
+static mut receiverID: ChatId = teloxide::prelude::ChatId(0);
+ 
+
 #[tokio::main]
 async fn main() {
     println!("initializing..");
@@ -10,9 +15,37 @@ async fn main() {
     
     println!("started..");
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        println!("inside!!!: {:?}", msg.text());
-        bot.send_message(msg.chat.id, "kill me, plz").await?;
+        let msg_text = msg.text();
+        let chat_id = msg.chat.id;
+        match msg_text {
+            None => println!("{} -> <empty>", chat_id),
+            Some(text) => {
+                println!("{} -> {}", chat_id, text);
+              unsafe{
+                match text {
+                    "/sender" => {
+                        senderID = chat_id;
+                        bot.send_message(chat_id, "you are sender").await?;
+                    },
+                    "/receiver" => {
+                        receiverID = chat_id;
+                        bot.send_message(chat_id, "you are receiver").await?;
+                    },
+                    _ => {
+                        if receiverID == teloxide::prelude::ChatId(0){
+                            bot.send_message(chat_id, "there is not receiver").await?;
+                        }else{
+                            bot.send_message(receiverID, text).await?;
+                        }
+                    },
+                }
+              }
+            },
+        }
         Ok(())
     })
     .await;
 }
+
+
+
